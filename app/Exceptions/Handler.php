529a447e7d74e9c5ce4b414e89080d5ce6c9efe5
OEM Configuration (temporary user) <crojasco@cientifica.edu.pe>
2019-08-04 11:27:58 -0500

    validar errores provenientes de los microservicios en el gateway

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,15 +2,20 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponser;
 use Exception;
-use Illuminate\Validation\ValidationException;
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
+
     /**
      * A list of the exception types that should not be reported.
      *
@@ -45,6 +50,52 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        return parent::render($request, $e);
+        //valida que el metodo de request sea el correcto 
+        if ($e instanceof HttpException) {
+            $code = $e->getStatusCode();
+            $message = Response::$statusTexts[$code];
+
+            return $this->errorResponse($message, $code);
+        }
+
+        //Valida los errores al no encontrar un id enviado
+        if ($e instanceof ModelNotFoundException) {
+            $model = strtolower(class_basename($e->getModel()));
+
+            return $this->errorResponse("No se encontró ningun registro de {$model} con el id enviado", Response::HTTP_NOT_FOUND);
+        }
+
+        //Valida los errores de autorisacion
+        if ($e instanceof AuthorizationException) {            
+            return $this->errorResponse($e->getMessage(), Response::HTTP_FORBIDDEN);
+        }
+
+        //Valida los errores de autenticacion
+        if ($e instanceof AuthenticationException) {            
+            return $this->errorResponse($e->getMessage(), Response::HTTP_UNAUTHORIZED);
+        }
+
+        if ($e instanceof ValidationException) {
+            $errors = $e->validator->errors()->getMessages();
+
+            return $this->errorResponse($errors, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        //controlar error de la API externa, en este caso el gateway actua como cliente
+        if ($e instanceof ClientException) {
+            $message = $e->getResponse()->getBody();
+            $code = $e->getCode();
+
+            return $this->errorMessage($message, $code);
+        }
+
+
+        //valida si la aplicacion esta en modo desarrollo para mostrar el error con mas detalles
+        if (env('APP_DEBUG', false)) {
+            return parent::render($request, $e);    
+        }
+
+        return $this->errorResponse('Error inesperado. Intente nuevamente', Response::HTTP_INTERNAL_SERVER_ERROR);
+        
     }
 }
