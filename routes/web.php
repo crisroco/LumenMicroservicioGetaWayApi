d3c77c35baab70806e95e9d013e641383abe51d2
OEM Configuration (temporary user) <crojasco@cientifica.edu.pe>
2019-08-04 13:26:11 -0500

    agrupar rutas y asignarle la seguridad

--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,28 @@
 |
 */
 
-/**
- * Author routers
- */
 
-$router->get('/authors','AuthorController@index');
-$router->post('/authors','AuthorController@store');
-$router->get('/authors/{author}','AuthorController@show');
-$router->put('/authors/{author}','AuthorController@update');
-$router->patch('/authors/{author}','AuthorController@update');
-$router->delete('/authors/{author}','AuthorController@destroy');
+$router->group(['middleware' => 'client.credentials'], function() use ($router){
+	/**
+	 * Author routers
+	 */
 
-/**
- * Book routers
- */
+	$router->get('/authors','AuthorController@index');
+	$router->post('/authors','AuthorController@store');
+	$router->get('/authors/{author}','AuthorController@show');
+	$router->put('/authors/{author}','AuthorController@update');
+	$router->patch('/authors/{author}','AuthorController@update');
+	$router->delete('/authors/{author}','AuthorController@destroy');
 
-$router->get('/books','BookController@index');
-$router->post('/books','BookController@store');
-$router->get('/books/{book}','BookController@show');
-$router->put('/books/{book}','BookController@update');
-$router->patch('/books/{book}','BookController@update');
-$router->delete('/books/{book}','BookController@destroy');
+	/**
+	 * Book routers
+	 */
+
+	$router->get('/books','BookController@index');
+	$router->post('/books','BookController@store');
+	$router->get('/books/{book}','BookController@show');
+	$router->put('/books/{book}','BookController@update');
+	$router->patch('/books/{book}','BookController@update');
+	$router->delete('/books/{book}','BookController@destroy');
+
+});	
